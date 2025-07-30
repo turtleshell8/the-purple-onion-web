f6342d6bf288ced83c65641e60d007ff1ed2993a
turtleshell8 <adamparuch4@gmail.com>
2025-07-30 10:49:13 -0700

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-console.log("hello there person what are you doing here? huh go back to hacking");
-window.alert("close me");
-//hey you stop looking at the code i know you
-//can see this in the javascript so leave NOW
