5dc68b7b5a7b9d62095a51a232027e0f82ca2e1e
turtleshell8 <adamparuch4@gmail.com>
2025-07-30 10:46:22 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("hello there person what are you doing here? huh go back to hacking");
+window.alert("close me");
+//hey you stop looking at the code i know you
+//can see this in the javascript so leave NOW
